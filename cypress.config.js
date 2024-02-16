32da869f56eb297d249b4ab9e2ae5f5a25d4b513
deborahcrstna <deborahcrstna@gmail.com>
2024-02-15 21:26:30 -0300

    add

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implemente aqui os ouvintes de eventos do node
+     
     },
     chromeWebSecurity: false
   },
